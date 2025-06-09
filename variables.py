047c504addf3e1fea0924376a8d6000bdd2e872e
Aadhiraiscool <krishna.aadhiras@gmail.com>
2025-06-09 10:40:29 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,26 @@
+"""
+print("welcome to the world of programming")
+print(4)
+("Hello Python \n")
+print("Hello",5)
+print("welcome to ", end="*")
+
+#activity two
+x=5
+y="john"
+print(x)
+print(y)
+
+codingal="codingal"
+print(codingal)
+
+name= input ("enter your name: ")
+
+print ("\nHello", name, "\nwelcome to codingal")
+print(name) 
+"""
+
+import keyword
+# print all the python keywords 
+print("Python keywords are...\n")
+print(keyword.kwlist)
