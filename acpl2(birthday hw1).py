fbddf0ab5b745a58189f75be9cacbe9620739a50
Aadhiraiscool <krishna.aadhiras@gmail.com>
2025-06-10 15:00:45 -0500

    Add files via upload

---
+++ acpl2(birthday hw1).py
@@ -0,0 +1,6 @@
+Birthday1="Iniyaa May 30"
+Birthday2="Raana May 2"
+Birthday3="Dhakshan May 29"
+Birthday4="Adharva April 13"
+Birthday5="Shanthi athai August 21"
+print(Birthday1, Birthday2, Birthday3,Birthday4, Birthday5 )
