b97b0fd0b9fc3db3b54f08556a6177a48014dcbf
Aadhiraiscool <krishna.aadhiras@gmail.com>
2025-06-27 10:52:48 -0500

    Add files via upload

---
+++ greater or smaller.py
@@ -0,0 +1,9 @@
+i=int(input("enter a number : "))
+if (i<15):
+    print("i is smaller than 15")
+    print("im in if Block")
+
+else:
+    print("i is greater than 15")
+    print("im in else BLock")
+print("im not in if and not in else Block")
