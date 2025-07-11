d1125c76c1393dc25e40bc08470c303098db78cb
Aadhiraiscool <krishna.aadhiras@gmail.com>
2025-07-11 10:16:08 -0500

    Add files via upload

---
+++ acpl5(checking alphabets hw4).py
@@ -0,0 +1,8 @@
+inp1=input("enter a character to know if it is an alphabet or not")
+
+if inp1>='a' and inp1<='z':
+    print("entered character,",inp1, "is an alphabet")
+elif inp1>='A' and inp1<='Z':
+    print(" entered character",inp1,"is an alphabet")
+else:
+    print("this is not an alphabet")
