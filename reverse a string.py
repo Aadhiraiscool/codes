4cf3ba7a362dfe72337091e9e3a518c7d268df93
Aadhiraiscool <krishna.aadhiras@gmail.com>
2025-07-14 10:38:15 -0500

    Add files via upload

---
+++ reverse a string.py
@@ -0,0 +1,12 @@
+#input a word or sentence 
+string=input ("Please enter your own string: ")
+
+
+string2=(' ')
+#loop for printing in reverse 
+for i in string:
+    string2=i+string2
+
+
+print("\nThe original string=",string)
+print("the reverse string=",string2)
