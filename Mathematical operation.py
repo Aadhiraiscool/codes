898b069e17e8649ab751178d0d2c5bf5fccbfde7
Aadhiraiscool <krishna.aadhiras@gmail.com>
2025-08-27 21:13:28 -0500

    Add files via upload

---
+++ Mathematical operation.py
@@ -0,0 +1,14 @@
+import math
+print('The Floor and ceiling calue of 23.56 are: ' + str(math.ceil(23.56)))
+
+x=10
+y=-15
+
+print('The calue of x after copying the sign from y is: '+
+      str(math.copysign(x,y)))
+
+print('Abosilute calue of -96 and 56 are: ' +
+      str(math.fabs(-96))+ str(math.fabs(56)))
+
+print('The GCD of 24 and 56 : ' + str(math.gcd(24,56)))
+print(math.sqrt(9))
